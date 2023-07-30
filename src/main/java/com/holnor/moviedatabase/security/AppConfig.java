b194117d6925cbdf006bdc60b7a8480ba70d06ca
holczur <holcz.norbert@gmail.com>
2023-07-30 18:51:33 +0200

    UserDetailsService was configured as Bean and now is used in doFilterInternal()

---
+++ src/main/java/com/holnor/moviedatabase/security/AppConfig.java
@@ -0,0 +1,23 @@
+package com.holnor.moviedatabase.security;
+
+import com.holnor.moviedatabase.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+
+}
