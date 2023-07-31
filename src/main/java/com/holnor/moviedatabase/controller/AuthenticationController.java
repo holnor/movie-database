e1b39dc5c17872ec517fe4d5b3b73589dba54be5
holczur <holcz.norbert@gmail.com>
2023-07-31 10:23:10 +0200

    endpoint renamed (authenticate -> login)

--- src/main/java/com/holnor/moviedatabase/controller/AuthenticationController.java
+++ src/main/java/com/holnor/moviedatabase/controller/AuthenticationController.java
@@ -25,7 +25,7 @@ public class AuthenticationController {
 
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
