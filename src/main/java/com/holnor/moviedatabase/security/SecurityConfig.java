2bd6d49303d42694cf96b4ca9bc38cdc326dad85
holczur <holcz.norbert@gmail.com>
2023-07-31 09:17:03 +0200

    whitelist was defined

--- src/main/java/com/holnor/moviedatabase/security/SecurityConfig.java
+++ src/main/java/com/holnor/moviedatabase/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")//TODO: whitelist
+                .requestMatchers("/api/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
