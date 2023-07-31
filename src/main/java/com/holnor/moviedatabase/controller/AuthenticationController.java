22a44970dc13d2e097ae8c032a505bd1e5468d78
holczur <holcz.norbert@gmail.com>
2023-07-31 08:33:22 +0200

    controller that handles register and login

---
+++ src/main/java/com/holnor/moviedatabase/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.holnor.moviedatabase.controller;
+
+import com.holnor.moviedatabase.security.auth.AuthenticationRequest;
+import com.holnor.moviedatabase.security.auth.AuthenticationResponse;
+import com.holnor.moviedatabase.security.auth.AuthenticationService;
+import com.holnor.moviedatabase.security.auth.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authenticationService.register(request));
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
